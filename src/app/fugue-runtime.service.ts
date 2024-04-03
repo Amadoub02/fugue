e5f9904b9487c4d181ea9e6fcb8c15db47188d6a
Nick Celestin Zizic <nick.celestin.zizic@gmail.com>
2024-04-03 15:45:37 -0400

    added worker

--- src/app/fugue-runtime.service.ts
+++ src/app/fugue-runtime.service.ts
@@ -253,3 +253,15 @@ private writeToConsoleLog(str: string, to_standard_error: boolean) {
     return outIdx - startIdx;
   }
 }
+
+if (typeof Worker !== 'undefined') {
+  // Create a new
+  const worker = new Worker(new URL('./fugue.worker', import.meta.url));
+  worker.onmessage = ({ data }) => {
+    console.log(`page got message: ${data}`);
+  };
+  worker.postMessage('hello');
+} else {
+  // Web Workers are not supported in this environment.
+  // You should add a fallback so that your program still executes correctly.
+}
