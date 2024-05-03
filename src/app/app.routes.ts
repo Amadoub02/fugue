4a770b2b52d12f8a61feeb609f0c1cd2cb1bb555
Joshua Angulo <jangulo@albany.edu>
2024-05-03 17:08:25 -0400

    Fixed routing bug

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,10 @@ import { Routes } from '@angular/router';
 
 import { EditorComponent } from './editor/editor.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { LoginComponent } from './login/login.component';
 import { TutorialsComponent } from './tutorials/tutorials.component';
 
 export const routes: Routes = [
     { path: '', component: HomepageComponent },
     { path: 'editor', component: EditorComponent},
-    { path: 'login', component: LoginComponent },
     { path: 'tutorials', component: TutorialsComponent }
 ];
