40a98415469910d3f92846de5b4c287cc0022e6a
Joshua Angulo <jangulo@albany.edu>
2024-05-03 19:12:45 -0400

    Fixed Build Errors

---
+++ extra-webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    node: {
+      global: true,
+      crypto: 'empty',
+      process: true,
+      module: false,
+      clearImmediate: false,
+      setImmediate: false,
+      fs: 'empty'
+    }
+  };
+  
